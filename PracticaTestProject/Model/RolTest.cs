1f56d15a00d8b42cfabcf60ece429e0da6a01ba6
Diego <dalonsos2004@gmail.com>
2025-11-12 11:56:23 +0100

    Creados Tests Agregar Permiso

--- PracticaTestProject/Model/RolTest.cs
+++ PracticaTestProject/Model/RolTest.cs
@@ -2,45 +2,88 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Practica.Model;
 using System;
 using System.Collections.Generic;
-using Assert; 
+using Assert;
 
-[TestClass]
-public class RolTests
+namespace PracticaTestProject.Model
 {
-    #region Pruebas del Constructor
-
-    [TestMethod]
-    public void Constructor_DatosValidos_AsignaPropiedadesCorrectamente()
+    [TestClass]
+    public class RolTests
     {
-        string nombre = "Admin";
-        string descripcion = "Rol de administrador";
+        #region Pruebas del Constructor
 
-        Rol rol = new Rol(nombre, descripcion);
+        [TestMethod]
+        public void Constructor_DatosValidos_AsignaPropiedadesCorrectamente()
+        {
+            string nombre = "Admin";
+            string descripcion = "Rol de administrador";
 
-        Assert.AreEqual(nombre, rol.Nombre);
-        Assert.AreEqual(descripcion, rol.Descripcion);
+            Rol rol = new Rol(nombre, descripcion);
+
+            Assert.AreEqual(nombre, rol.Nombre);
+            Assert.AreEqual(descripcion, rol.Descripcion);
+        }
+        [TestMethod]
+        public void Constructor_DatosValidos_InicializaHashSetPermisosVacio()
+        {
+            string nombre = "Editor";
+            string descripcion = "Rol de editor";
+
+            Rol rol = new Rol(nombre, descripcion);
+
+            Assert.IsNotNull(rol.ObtenerPermisos());
+            Assert.AreEqual(0, rol.ObtenerPermisos().Count);
+        }
+
+        [DataTestMethod]
+        [DataRow(null)]
+        [DataRow("")]
+        [DataRow("   ")]
+        public void Constructor_NombreNuloOVacio_LanzaArgumentException(string nombre)
+        {
+            string descripcion = "Descripción válida";
+
+            Assert.ThrowsException<ArgumentException>(() => new Rol(nombre, descripcion));
+        }
+
+        #region Pruebas del Método AgregarPermiso
+        [TestMethod]
+        public void AgregarPermiso_PermisoValido_AñadePermisoAHashSet()
+        {
+            Rol rol = new Rol("Tester", "Rol de pruebas");
+            string permiso = "ejecutar_pruebas";
+
+            rol.AgregarPermiso(permiso);
+
+            Assert.AreEqual(1, rol.ObtenerPermisos().Count);
+            Assert.IsTrue(rol.TienePermiso(permiso));
+        }
+
+        [DataTestMethod]
+        [DataRow(null)]
+        [DataRow("")]
+        [DataRow("   ")]
+        public void AgregarPermiso_PermisoNuloOVacio_LanzaArgumentException(string permiso)
+        {
+            Rol rol = new Rol("Guest", "Rol de invitado");
+
+            Assert.ThrowsException<ArgumentException>(() => rol.AgregarPermiso(permiso));
+        }
+
+        [TestMethod]
+        public void AgregarPermiso_PermisoDuplicado_HashSetIgnoraDuplicadoSinExcepcion()
+        {
+            Rol rol = new Rol("Developer", "Rol de desarrollador");
+            string permiso = "escribir_codigo";
+            rol.AgregarPermiso(permiso);
+
+            rol.AgregarPermiso(permiso);
+
+
+            Assert.AreEqual(1, rol.ObtenerPermisos().Count);
+            Assert.IsTrue(rol.TienePermiso(permiso));
+        }
+
+
+
     }
-    [TestMethod]
-    public void Constructor_DatosValidos_InicializaHashSetPermisosVacio()
-    {
-        string nombre = "Editor";
-        string descripcion = "Rol de editor";
-
-        Rol rol = new Rol(nombre, descripcion);
-
-        Assert.IsNotNull(rol.ObtenerPermisos());
-        Assert.AreEqual(0, rol.ObtenerPermisos().Count);
-    }
-
-    [DataTestMethod]
-    [DataRow(null)]
-    [DataRow("")]
-    [DataRow("   ")]
-    public void Constructor_NombreNuloOVacio_LanzaArgumentException(string nombre)
-    {
-        string descripcion = "Descripción válida";
-
-        Assert.ThrowsException<ArgumentException>(() => new Rol(nombre, descripcion));
-    }
-
 }
