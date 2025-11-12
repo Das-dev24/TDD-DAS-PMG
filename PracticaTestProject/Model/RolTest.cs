6a5b06e304cbe4175ee624d0859d92b77b8fe8f7
Diego <dalonsos2004@gmail.com>
2025-11-12 11:46:37 +0100

    Creados tests constructor

--- PracticaTestProject/Model/RolTest.cs
+++ PracticaTestProject/Model/RolTest.cs
@@ -1,10 +1,46 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Model;
+using Practica.Model;
 using System;
 using System.Collections.Generic;
+using Assert; 
 
 [TestClass]
 public class RolTests
 {
+    #region Pruebas del Constructor
+
+    [TestMethod]
+    public void Constructor_DatosValidos_AsignaPropiedadesCorrectamente()
+    {
+        string nombre = "Admin";
+        string descripcion = "Rol de administrador";
+
+        Rol rol = new Rol(nombre, descripcion);
+
+        Assert.AreEqual(nombre, rol.Nombre);
+        Assert.AreEqual(descripcion, rol.Descripcion);
+    }
+    [TestMethod]
+    public void Constructor_DatosValidos_InicializaHashSetPermisosVacio()
+    {
+        string nombre = "Editor";
+        string descripcion = "Rol de editor";
+
+        Rol rol = new Rol(nombre, descripcion);
+
+        Assert.IsNotNull(rol.ObtenerPermisos());
+        Assert.AreEqual(0, rol.ObtenerPermisos().Count);
+    }
+
+    [DataTestMethod]
+    [DataRow(null)]
+    [DataRow("")]
+    [DataRow("   ")]
+    public void Constructor_NombreNuloOVacio_LanzaArgumentException(string nombre)
+    {
+        string descripcion = "Descripción válida";
+
+        Assert.ThrowsException<ArgumentException>(() => new Rol(nombre, descripcion));
+    }
 
 }
