9d96088afe504b629e3e2ade39be36bc9dd543f7
Pablito2442 <pmatego@gmail.com>
2025-11-21 15:32:13 +0100

    Creacion de Contructor Clase App

--- Practica/Model/App.cs
+++ Practica/Model/App.cs
@@ -5,6 +5,28 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Practica.Model {
-    internal class App {
+    public class App {
+
+        public Usuario UsuarioActual { get; private set; }
+        public Dictionary<Proyecto, HashSet<string>> PermisosPorProyecto { get; private set; }
+
+        public App(Usuario usuario, List<Proyecto> todosLosProyectos) {
+            if (usuario == null)
+                throw new ArgumentNullException(nameof(usuario), "El usuario no puede ser nulo.");
+
+            if (todosLosProyectos == null)
+                throw new ArgumentNullException(nameof(todosLosProyectos), "La lista de proyectos no puede ser nula.");
+
+            UsuarioActual = usuario;
+            PermisosPorProyecto = new Dictionary<Proyecto, HashSet<string>>();
+
+            // Procesar accesos para cada proyecto listado
+            foreach (var proyecto in todosLosProyectos) {
+                // Obtenemos la lista de permisos (List<string>) desde el Proyecto
+                var listaPermisos = proyecto.ObtenerPermisosDeUsuario(usuario);
+
+                PermisosPorProyecto[proyecto] = new HashSet<string>(listaPermisos);
+            }
+        }
     }
 }
